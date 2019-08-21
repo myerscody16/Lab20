98acf8a80e0ee1b115091a0ce6bbec9c4254c87e
myerscody16 <myerscody16@live.com>
2019-08-20 20:55:53 -0400

    initial

---
+++ Lab20/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab20.Models;
+
+namespace Lab20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult RegisterNewUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RegisterNewUser(RegisterUser newUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(newUser);
+            }
+            else
+            {
+                return View("AddNewUser", newUser);
+            }
+        }
+        public IActionResult AddNewUser()
+        {
+
+        }
+    }
+}
---
+++ Lab20/Models/RegisterUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab20.Models
+{
+    public class RegisterUser
+    {
+        [Required]
+        [StringLength(30, ErrorMessage = "Must be between 3 and 30 characters", MinimumLength = 3)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "Must be between 3 and 30 characters", MinimumLength = 3)]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime Birthday { get; set; }
+        [Required]
+        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)")]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "Must be between 3 and 30 characters", MinimumLength = 3)]
+        public string Password { get; set; }
+        [Required]
+        public string FavoriteColor { get; set; }
+
+
+    }
+}
